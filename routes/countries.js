d2e26ff218be852555dd05d801bbb4772cbca249
Yoah98 <joshfell98@gmail.com>
2022-06-12 15:08:30 +1000

    add error handling to countries route

--- routes/countries.js
+++ routes/countries.js
@@ -2,19 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
+  console.log(req.query);
+  console.log(Object.keys(req.query).length);
+
+  if (Object.keys(req.query).length > 0){
+    res.json({"error": true, "message": "Invalid query parameters. Query parameters are not permitted."});
+  }
+
+  else{
     req.db.from('data').distinct('country').orderBy('country')
     .then((rows) => {
       res.status(200).json(rows.map((country) => country.country));
-      
     })
-    .catch(err => {
-      console.log(err);
-      res.json({
-        Error: true,
-        Message: "Invalid query parameters. Query parameters are not permitted."
-      })
-    })
-  })
+  }
+});
 
   module.exports = router;
   
